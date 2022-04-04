f9c255a460d02771e457f95a8c7aba2a57bc3d48
Marie Howet <howet.marie@gmail.com>
2022-04-04 14:36:13 +0200

    hey

--- main/src/View/Main.java
+++ main/src/View/Main.java
@@ -4,6 +4,7 @@ package View;
 public class Main {
     public static void main(String[] args) {
         MainJFrame  mainJFrame = new MainJFrame();
+        System.out.println("Coucou Melmel !!!");
     }
 
 }
