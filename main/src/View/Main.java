e28fc6045e89f748c52b6bb1e4ccca7f6afdcb5b
Melvin <melvinparache@gmail.com>
2022-04-04 14:29:24 +0200

    Test de ouf

--- main/src/View/Main.java
+++ main/src/View/Main.java
@@ -1,7 +1,11 @@
 package View;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         MainJFrame  mainJFrame = new MainJFrame();
+        System.out.println("Hey girl it's Melvin do you want to call me ? <3");
     }
+
 }
