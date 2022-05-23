1bc3013e34ee650dacf29e6d87950830c29ec8af
Melvin <melvinparache@gmail.com>
2022-05-23 16:06:44 +0200

    Modif Test

--- main/src/Tests/Test.java
+++ main/src/Tests/Test.java
@@ -19,20 +19,20 @@ class Test {
     @org.junit.jupiter.api.Test
     public void add() throws FlightPriceException, SeatTypeException, PriceException {
         Double[] priceWeights = {0.0,10.0,20.0,25.0};
-        Double priceExpected = 750.0 + 100.0 + 20.0;
+        Double priceExpected = 100.0 + 100.0 + 20.0;
         assertEquals(priceExpected, calculator.add(manager.getFlightPrice(1),(double)manager.getActualSeatType(7).getAdditionalPrice(),priceWeights[2]), 0.01);
     }
 
     @org.junit.jupiter.api.Test
     public void getSeatTypes() throws PriceException, AllSeatTypesException {
-        Object [] seatTypesExpected = {new SeatType("Economic", 30),new SeatType("Business", 100), new SeatType("First", 200)};
+        Object [] seatTypesExpected = {new SeatType("Economique", 30),new SeatType("Affaire", 100), new SeatType("Premiere", 200)};
         Object [] seatTypesActual =  manager.getAllSeatTypes().toArray();
         assertArrayEquals(seatTypesExpected, seatTypesActual);
     }
 
     @org.junit.jupiter.api.Test
     public void getFlightPrice() throws FlightPriceException {
-        Double priceExpected = 750.0;
+        Double priceExpected = 100.0;
         Double priceActual = manager.getFlightPrice(1);
         assertEquals(priceExpected, priceActual);
     }
